b63f81901d445101760afceb8f08bfacf05aaaf7
Abrar74774 <abrarshahriarhossain@gmail.com>
2024-02-15 16:06:14 +0300

    Switch to multiple browser instances instead of tabs

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const puppeteer = require('puppeteer')
+
 
 const users = [
     {
-        name: process.env.user1,
-        password: process.env.pass1
+        name: process.env.USER1,
+        password: process.env.PASS1
     },
     {
-        name: process.env.user2,
-        password: process.env.pass2
+        name: process.env.USER2,
+        password: process.env.PASS2
     },
 ]
 
@@ -22,50 +25,31 @@ const titleInput = "input#RPE_txtArea"
 const subTitleInput = "input#RPE_txtAreaSTitle"
 const submitBtn = "dialog div.form-group:nth-child(4) > button:nth-child(1)"
 
-function delay(time) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time)
-    });
-}
-
-async function waitUntilDownload(page, fileName = '') {
-    return new Promise((resolve, reject) => {
-        page._client().on('Page.downloadProgress', e => { // or 'Browser.downloadProgress'
-            if (e.state === 'completed') {
-                resolve(fileName);
-            } else if (e.state === 'canceled') {
-                reject();
-            }
-        });
-    });
-}
 
 async function printPDF() {
     console.log(process.cwd())
-    const browser = await puppeteer.launch({ 
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-            '--disable-gpu'
-          ],
-        headless: false 
-    });
-
-    const page = await browser.newPage();
-    const client = await page.target().createCDPSession()
-    await client.send('Page.setDownloadBehavior', {
-        behavior: 'allow',
-        downloadPath: process.cwd(),
-    })
-    await page.close()
-    const one = [users[0]]
-    one.forEach(async (user) => {
+    // const one = [users[1]]
+    users.forEach(async (user) => {
+        const browser = await puppeteer.launch({ 
+            // args: [
+            //     '--no-sandbox',
+            //     '--disable-setuid-sandbox',
+            //     '--disable-dev-shm-usage',
+            //     '--disable-accelerated-2d-canvas',
+            //     '--no-first-run',
+            //     '--no-zygote',
+            //     '--single-process',
+            //     '--disable-gpu'
+            //   ],
+            headless: true 
+        });
+    
         const page = await browser.newPage();
+        const client = await page.target().createCDPSession()
+        await client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: process.cwd() + `\\${user.name}`,
+        })
         await page.setViewport({ width: 1920, height: 1080 });
         await page.goto('https://sedgedev.solverminds.net/auth/login', { waitUntil: 'networkidle0', timeout: 120000 });
     
@@ -90,6 +74,7 @@ async function printPDF() {
     
         await page.waitForNetworkIdle({idleTime: 5000})
         await page.close()
+        await browser.close()
     })
 
 
